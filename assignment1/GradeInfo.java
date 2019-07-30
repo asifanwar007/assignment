197059348cf551d66623f19ff139cb9cacaa8b05
asifanwar007 <33431458+asifanwar007@users.noreply.github.com>
2019-07-30 17:11:16 +0530

    add files in col106 lab

---
+++ assignment1/GradeInfo.java
@@ -0,0 +1,52 @@
+import java.util.Iterator;
+import java.io.*;
+
+public class GradeInfo implements GradeInfo_{
+	String grade;
+	public GradeInfo(String grade){
+		this.grade = grade;
+	}
+	
+	enum LetterGrade{
+		A, Aminus, B, Bminus, C, Cminus, D, E, F, I;
+	}
+	
+	
+	public static int gradepoint(GradeInfo.LetterGrade grade){
+		int a = 0;
+		switch (grade){
+		case A:
+			a = 10;
+			break;
+		case Aminus:
+			a =  9;
+			break;
+		case B:
+			a = 8;
+			break;
+		case Bminus:
+			a = 7;
+			break;
+		case C:
+			a = 6;
+			break;
+		case Cminus:
+			a = 5;
+			break;
+		case D:
+			a = 4;
+			break;
+		case E:
+			a = 3;
+			break;
+		case F:
+			a = 2;
+			break;
+		default:
+			a = 0;
+		
+		}
+		return a;
+	}
+}
+	
---
+++ assignment1/Student.java
@@ -0,0 +1,12 @@
+import java.util.Iterator;
+import java.io.*;
+
+public class Student implements Student_{
+	String name;
+	String entryNo;
+	String hostel;
+	String department;
+	String completedCredits;
+	String cgpa;
+	
+}
